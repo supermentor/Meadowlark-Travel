c8e1050b9cb8d8e14f0df94318b02d84bc7738d1
Artur Barbosa <arturbarbosa10@gmail.com>
2019-06-29 15:29:57 -0300

    updated directory with some modules

---
+++ node.js
@@ -0,0 +1,33 @@
+var http = require("http"),
+    fs = require("fs");
+
+function serveStaticFile(res, path, contentType, responseCode){
+    if(!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function(err, data){
+        if(err){
+            res.writeHead(500, {'Content-Type': "text/plain"});
+            res.end('500 - Internal Error');
+        }
+        else{
+            res.writeHead(responseCode, {"Content-Type" : contentType});
+            res.end(data);
+        }
+    });
+}
+
+http.createServer(function(req, res){
+    var path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
+    switch(path){
+        case "":
+            serveStaticFile(res, '/public/index.html', 'text/html');
+            break;
+        case "/img/logo.png":
+            serveStaticFile(res, '/img/logo.png', 'image/png');
+            break;
+        default:
+            res.writeHead(404, {"Content-Type" : "text/plain"});
+            res.end("404 - Not Found");
+    }
+}).listen(3000);
+
+console.log("Server started on localhost:3000; press Ctrl-C to terminate...");
