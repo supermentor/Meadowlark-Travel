2111c16e78b797ef518774bcbcd2f72691ce5fbd
Artur Barbosa <arturbarbosa10@gmail.com>
2019-06-30 18:53:55 -0300

    moved 'fortune cookie' functionality into a separate module

---
+++ meadowlark.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var app = express();
+var fortune = require('./lib/fortune.js');
+
+// set up handlebars view engine
+var handlebars = require('express3-handlebars')
+ .create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+//set default port
+app.set('port', process.env.PORT || 3000);
+
+//Static Files
+app.use(express.static(__dirname + '/public'));
+
+//Home Page
+app.get("/", function(req, res){
+    res.render("home");
+});
+   
+//About us page
+app.get("/about", function(req, res){
+    res.render("about", {fortune: fortune.getFortune()});
+});
+
+//custom 404 page
+app.use(function(req, res){
+    res.status(404);
+    res.render("404");
+});
+
+//custom 500 page
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.render("500");
+});
+
+app.listen(app.get("port"), function(){
+    console.log("Express started on localhost:" + app.get("port") + ". Press Ctrl + C to terminate...");
+});
